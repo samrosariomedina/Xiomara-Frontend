9151c5e3dc1f006848baa57e5dabb182969f53d8
ahmadali507 <ahmad.ali.000507@gmail.com>
2025-09-12 11:03:51 +0500

    feat: folder strucutre improved and redundant files removed

--- app/[locale]/clients/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function ClientsLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <div>{children}</div>
-  )
-}
--- app/[locale]/content-engine/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function ContentEngine({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <div>{children}</div>
-  )
-}
